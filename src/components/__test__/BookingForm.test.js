cbcdd3d1f8ee7c028ffd060ce98311d043ae2f96
Sukeerthi123 <116960224+Sukeerthi123@users.noreply.github.com>
2023-03-31 23:02:21 +0530

    Update BookingForm.test.js

--- src/components/__test__/BookingForm.test.js
+++ src/components/__test__/BookingForm.test.js
@@ -1,4 +1,8 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 
 import BookingForm from "../BookingForm";
-
+test("Renders the BookFrontPage", () => {
+    render(<BookFrontPage />);
+    const submitBtn = screen.getByText("Make Reservation");
+    expect(submitBtn).toBeInTheDocument();
+  });
